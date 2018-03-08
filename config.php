bc2996785e0c5e44aca0d9073c1ed4826764252d
Simeon Adedokun <15355542+osparkle@users.noreply.github.com>
2018-03-08 04:41:48 +0100

    Update config.php

--- config.php
+++ config.php
@@ -4,6 +4,6 @@
 	//define Username and Password if connecting directly to your main account
 	define("USERNAME", "your_orgs_username");
 	define("PASSWORD", "your_orgs_password");
-	//define API_TOKEN if connecting to an APIX-X account 
-	define("API_TOKEN", "");//can be created at http://orgds.org/createapix
+	//define API_TOKEN if connecting to an APIX-X account, created at http://orgds.org/createapix
+	define("API_TOKEN", "");
 ?>

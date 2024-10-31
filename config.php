4c088666847362a7d2c994d9a28644c9638ddc6e
Simeon Adedokun <15355542+osparkle@users.noreply.github.com>
2024-10-31 17:31:23 +0100

    Update config.php
    Change of domain name

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-	define("API_URL", "http://orgds.org/api");
+	define("API_URL", "http://orgds.com.ng/api");
 	//you can connect to the API URL with either API TOKEN or your Login Username and Password
 	//define Username and Password if connecting directly to your main account
 	define("USERNAME", "your_orgs_username");
